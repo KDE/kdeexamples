ffb0dbf68fc2c4451cc410bd55904907f26947c9
Aaron J. Seigo <aseigo@kde.org>
2010-01-08 21:32:58 +0000

    license headers
    svn path=/trunk/KDE/kdeexamples/; revision=1071833

--- plasma/c++/plasmoids/animation/PlasmaAnimationExample.cpp
+++ plasma/c++/plasmoids/animation/PlasmaAnimationExample.cpp
@@ -1,3 +1,31 @@
+/*
+ * Copyright 2009 Mehmet Akmanalp <makmanalp@wpi.edu>
+ * Copyright 2009 Adenilson Cavalcanti <savagobr@yahoo.com>
+ * Copyright 2009 Igor Trindade Oliveira <igor_trindade@yahoo.com.br>
+ * Copyright 2009 Marco Martin <notmart@gmail.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "PlasmaAnimationExample.h"
 
 #include <QPainter>
@@ -134,15 +162,18 @@ void PlasmaAnimationExample::init()
 
 void PlasmaAnimationExample::revertDirection()
 {
-    if (m_seqGroup->property("direction") == QAbstractAnimation::Forward)
+    if (m_seqGroup->property("direction") == QAbstractAnimation::Forward) {
         m_seqGroup->setProperty("direction", QAbstractAnimation::Backward);
-    m_seqGroup->start();
+    }
 
+    m_seqGroup->start();
 }
 void PlasmaAnimationExample::startAll()
 {
-    if (m_seqGroup->property("direction") == QAbstractAnimation::Backward)
+    if (m_seqGroup->property("direction") == QAbstractAnimation::Backward) {
         m_seqGroup->setProperty("direction", QAbstractAnimation::Forward);
+    }
+
     m_seqGroup->start();
 }
 
