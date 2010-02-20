31901beb18422541cdd453ec6f132d6176233202
Aaron J. Seigo <aseigo@kde.org>
2010-02-20 17:53:42 +0000

    fixed
    svn path=/trunk/KDE/kdeexamples/; revision=1093362

--- plasma/javascript/plasmoids/script-digital-clock/contents/code/main.js
+++ plasma/javascript/plasmoids/script-digital-clock/contents/code/main.js
@@ -2,11 +2,12 @@ layout = new LinearLayout(plasmoid);
 label = new Label();
 layout.addItem(label);
 
-dataEngine("time").connectSource("UTC", plasmoid, 500);
 //print(dataEngine("time").query("UTC").Time.toString());
 
 plasmoid.dataUpdate = function(a, b)
 {
     label.text = "It is " + b.Time.toString() + " in " + a;
 }
+
+dataEngine("time").connectSource("UTC", plasmoid, 500);
 
