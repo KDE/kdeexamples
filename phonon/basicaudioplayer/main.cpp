e7338cdd4d8613f2b5f59ae48f1dbd8f30ea47cd
Jon Ander Peñalba <jonan88@gmail.com>
2011-07-28 15:45:20 +0200

    New basic and simple audio player using Phonon.

---
+++ phonon/basicaudioplayer/main.cpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2011 Jon Ander Peñalba <jonan88@gmail.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <KAboutData>
+#include <KApplication>
+#include <KCmdLineArgs>
+
+#include "mainwindow.h"
+
+int main(int argc, char *argv[])
+{
+  KAboutData aboutData("basicaudioplayer", 0,
+                       ki18n("Basic Audio Player"), "1.0",
+                       ki18n("A basic audio player using Phonon."),
+                       KAboutData::License_BSD,
+                       ki18n("Copyright (c) 2011 Jon Ander Peñalba <jonan88@gmail.com>"));
+  KCmdLineArgs::init(argc, argv, &aboutData);
+  KApplication app;
+
+  MainWindow *window = new MainWindow();
+  window->show();
+
+  return app.exec();
+}
