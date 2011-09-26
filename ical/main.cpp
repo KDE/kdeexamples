61b3a9cacbc12845c02427de413318ab98a86b6c
Thorsten Staerk <dev@staerk.de>
2011-09-26 11:26:54 +0200

    adding ical demo

---
+++ ical/main.cpp
@@ -0,0 +1,33 @@
+/*
+This is a test case for KDE's iCalendar functionality.
+(c) 2008 by Thorsten Staerk
+*/
+
+#include <QString>
+#include <kapplication.h>
+#include <kaboutdata.h>
+#include <kcmdlineargs.h>
+#include <KMainWindow>
+#include <kcal/resourcecalendar.h>
+#include <kcal/resourcecached.h>
+#include <kcal/resourcelocal.h>
+
+int main (int argc, char *argv[])
+{
+  const QByteArray& ba=QByteArray("test");
+  const KLocalizedString name=ki18n("myName");
+  KAboutData aboutData( ba, ba, name, ba, name);
+  KCmdLineArgs::init( argc, argv, &aboutData );
+  KApplication khello;
+  KCal::ResourceCalendar* cal;
+  KCal::ResourceCached* resource;
+  resource=new KCal::ResourceLocal("/tmp/test.ics");
+  cal=resource;
+  cal->load();
+  KCal::Todo* todo1 = new KCal::Todo();
+  todo1->setSummary("test todo");
+  cal->addTodo(todo1);
+  KABC::Lock *lock = cal->lock();
+  cal->save();
+  lock->unlock();
+}
