61f24c6fd70fe3a9146882d570de048bafc4e670
Kurt Hindenburg <kurt.hindenburg@gmail.com>
2013-07-20 20:19:45 -0400

    Krazy fix: mark constructor as explicit

--- authorization/kauth_example/mainwindow.h
+++ authorization/kauth_example/mainwindow.h
@@ -41,7 +41,7 @@ class MainWindow : public QMainWindow
     QPushButton *pushButton;
 
 public:
-    MainWindow(QWidget *parent = 0);
+    explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
 private:
