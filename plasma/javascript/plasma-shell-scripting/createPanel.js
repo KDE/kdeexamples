cd1ee242d63f7e08a282c936f02eefc61acc89df
Marco Martin <notmart@gmail.com>
2013-09-03 13:58:49 +0200

    remove c++ and pure js plasmoids

--- plasma/javascript/plasma-shell-scripting/createPanel.js
+++
@@ -1,6 +0,0 @@
-panel = new Panel
-panel.location = "top"
-panel.length = 200
-panel.alignment = "center"
-sleep(2000)
-panel.remove()
--- plasma/javascript/plasma-shell-scripting/listingWallpaperPlugins.js
+++
@@ -1,7 +0,0 @@
-var wallpapers = wallpaperPlugins();
-for (i in wallpapers) {
-  print(i);
-  for (j in wallpapers[i]) {
-    print("    Mode: " + wallpapers[i][j]);
-  }
-}
